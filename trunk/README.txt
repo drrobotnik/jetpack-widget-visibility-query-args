7a07bdc4f48182cf123e58f2e3f648560f35878e
Brandon Lavigne <B@Brandons-MacBook-Pro.local>
2014-12-08 15:51:16 -0800

    initial commit

---
+++ trunk/README.txt
@@ -0,0 +1,67 @@
+=== Jetpack Widget Visibility - Query Args ===
+Contributors: drrobotnik
+Donate link: http://github.com/drrobotnik/
+Tags: widget, extend, jetpack, visibility
+Requires at least: 3.5.1
+Tested up to: 4.0.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin adds query args to the Jetpack Widget Visiblity module.
+
+== Description ==
+
+The Jetpack visiblity module is appended to the bottom of all widgets (including 3rd parties). This plugin adds a "Query Args" option so that you can add a key value pair. For example, show a promotional text widget when someone enters a specific search.
+
+== Installation ==
+
+= Using The WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Search for 'Jetpack Widget Visibility Extended'
+3. Click 'Install Now'
+4. Activate the plugin on the Plugin dashboard
+
+= Uploading in WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Navigate to the 'Upload' area
+3. Select `bugherd-dashboard.zip` from your computer
+4. Click 'Install Now'
+5. Activate the plugin in the Plugin dashboard
+
+= Using FTP =
+
+1. Download `bugherd-dashboard.zip`
+2. Extract the `bugherd-dashboard` directory to your computer
+3. Upload the `bugherd-dashboard` directory to the `/wp-content/plugins/` directory
+4. Activate the plugin in the Plugin dashboard
+
+== Frequently Asked Questions ==
+
+= This plugin doesn't appear to do anything =
+
+This plugin itself does not add any user facing features. It adds the capability for theme/plugin developers to add additional options/fields to the Jetpack Widget Visibility Module.
+
+= What do I need to do to add options or fields within my theme/plugin =
+
+This feature is not part of the official Jetpack plugin (yet). In order for this to work you must first install the "Jetpack Widget Visibility Extended" plugin.
+
+== Screenshots ==
+
+1. Display Widget only when search result equals this keyword
+![Search Example](./assets/search.png)
+
+2. set a custom query_var in your theme/plugin and it can be referenced within this key value pair.
+![Any query var available works](./assets/query-var.png)
+
+== Changelog ==
+
+= 1.0 =
+* Initial Release
+
+== Upgrade Notice ==
+
+= 1.0 =
+Good to go!
---
+++ trunk/jetpack-widget-visibility-query-args.php
@@ -0,0 +1,90 @@
+<?php /**
+ * Plugin Name: Jetpack Widget Visibility Additional Fields - Query Args
+ * Plugin URI: http://caavadesign.com
+ * Description: Add the ability to add query args to jetpacks visibility widget
+ * Version: 1.0.1
+ * Author: Brandon lavigne
+ * Author URI: http://caavadesign.com
+ * License: GPL2
+ */
+
+function jetpack_widget_visibility_qa_js() { ?>
+<script>
+jQuery(function($) {
+	$( document ).on( 'change.widgetconditions', 'select.conditions-rule-major', function() {
+		var $conditionsRuleMajor = $ ( this );
+		var $conditionsRuleMinor = $conditionsRuleMajor.siblings( 'select.conditions-rule-minor:first' );
+		var $conditionsRuleQueryVars = $conditionsRuleMajor.siblings( 'input.conditions-rule-query-var' );
+
+		if ( $conditionsRuleMajor.val() == 'query_vars' ) {
+			$conditionsRuleMinor.hide();
+			$conditionsRuleQueryVars.show();
+		}else{
+			$conditionsRuleMinor.show();
+			$conditionsRuleQueryVars.hide();
+			$conditionsRuleQueryVars.val("");
+		}
+	});
+});
+
+</script><?php
+}
+
+function jetpack_widget_visibility_qa_minor_visible( $default, $rule ) {
+	
+	if ( 'query_vars' == $rule['major'] ) {
+		return false;
+	}
+	return true;
+}
+
+function jetpack_widget_visibility_qa_condition_major( $rule ) { ?>
+<option value="query_vars" <?php selected( "query_vars", $rule['major'] ); ?>><?php esc_html_e( 'Query Vars', 'jetpack' ); ?></option>
+<?php }
+
+function jetpack_widget_visibility_qa_conditions( $conditions ) {
+    return array( 'major' => '', 'minor' => '', 'query_var_key' => '', 'query_var_value' => '' );
+}
+
+function jetpack_widget_visibility_qa_additional_fields( $rule ) { 
+	?><input class="conditions-rule-query-var small-text<?php if ( ! $rule['major'] || 'query_vars' != $rule['major'] ) { ?> hidden<?php } ?>" name="conditions[rules_query_var_key][]" value="<?php echo $rule['query_var_key']; ?>" data-brandon="" placeholder="key" />
+	<input class="conditions-rule-query-var small-text<?php if ( ! $rule['major'] || 'query_vars' != $rule['major'] ) { ?> hidden<?php } ?>" name="conditions[rules_query_var_value][]" value="<?php echo $rule['query_var_value']; ?>" data-brandon="" placeholder="value" /><?php 
+}
+
+function jetpack_widget_visibility_qa_conditions_defaults( $defaults, $index ) {
+	$defaults['query_var_key'] = isset( $_POST['conditions']['rules_query_var_key'][$index] ) ? $_POST['conditions']['rules_query_var_key'][$index] : '';
+	$defaults['query_var_value'] = isset( $_POST['conditions']['rules_query_var_value'][$index] ) ? $_POST['conditions']['rules_query_var_value'][$index] : '';
+	return $defaults;
+}
+
+
+function jetpack_widget_visibility_qa_filter_widget( $condition_result, $rule ) {
+
+	if( 'query_vars' == $rule['major']) {
+		if( ICL_LANGUAGE_CODE == $rule['query_var_value'] ) {
+			$condition_result = true;
+		} else {
+			$condition_result = false;
+		}
+
+	}
+
+	return $condition_result;
+}
+
+function jetpack_widget_visibility_qa_init() {
+	global $pagenow;
+	if( 'widgets.php' == $pagenow) {
+		add_action( 'admin_head', 'jetpack_widget_visibility_qa_js',100);
+	}
+}
+
+
+add_filter( 'widget_conditions_defaults', 'jetpack_widget_visibility_qa_conditions_defaults', 10, 2 );
+add_filter( 'widget_visibility_conditions', 'jetpack_widget_visibility_qa_conditions' );
+add_filter( 'widget_visibility_minor_visible', 'jetpack_widget_visibility_qa_minor_visible', 10, 2 );
+add_filter( 'widget_visibility_filter_widget', 'jetpack_widget_visibility_qa_filter_widget', 10, 2 );
+
+add_action('admin_init','jetpack_widget_visibility_qa_init');
+add_action( 'widget_visibility_condition_major', 'jetpack_widget_visibility_qa_condition_major' );
+add_action( "widget_visibility_additional_fields", 'jetpack_widget_visibility_qa_additional_fields' );
